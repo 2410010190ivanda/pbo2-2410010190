caf15ae0a8d3b22e9cf5dddef2bbd4991105f099
2410010190ivanda <aleeleeee@gmail.com>
2026-09-22 20:11:24 +0800

    menambahkan kartu mahasiswa

---
+++ pbo2-2410010190/src/main/java/id/ac/uniska/pbo2/p01/KartuMahasiswa.java
@@ -0,0 +1,30 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package id.ac.uniska.pbo2.p01;
+
+/**
+ *
+ * @author ivanda
+ */
+public class KartuMahasiswa {
+    public static void main(String[] args){
+        String nama = "Muhammad Ivanda Stevhany";
+        long npm = 2410010190L;
+        String prodi = "Teknik Informatika";
+        int semester = 5;
+        String alasan = "Ingin memahami apa itu pemrograman berbasis objek lebih lanjut";
+        
+        
+        System.out.println("===============================");
+        System.out.println("KARTU MAHASISWA PBO 2");
+        System.out.println("===============================");
+        System.out.println("Nama: " + nama);
+        System.out.println("NPM: " + npm);
+        System.out.println("Prodi: " + prodi);
+        System.out.println("Semester: " + semester);
+        System.out.println("Alasan: " + alasan);
+        System.out.println("===============================");
+    }
+}
